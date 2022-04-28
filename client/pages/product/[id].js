6fe3542f7341e7914fec17a7b6d67631819cdb54
chrisfeatley <chris.featley@yodel.co.uk>
2022-04-28 17:10:06 +0100

    Styling of product screen

--- client/pages/product/[id].js
+++ client/pages/product/[id].js
@@ -21,41 +21,47 @@ export default function Product({ product }) {
     colour } = product;
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '50vh'}}>
-      <div style={{ width: '50%', height: '50%', position: 'relative' }}>
-        <Image src={img_url} alt={name} layout={"fill"} objectFit={"contain"}/>
+    <main className="flex flex-col justify-between pt-8">
+      <div className="flex flex-col items-center bg-gradient-to-b from-blue-dark via-blue-dark to-blue pt-8">
+        <div className="w-60 h-60 relative">
+          <Image className="pl-8" src={img_url} alt={name} layout={"fill"} objectFit={"contain"}/>
+        </div>
+        <p className="text-sub-heading font-bold text-white px-8 self-start">{name}</p>
+        <p className="text-base text-blue-accent px-8 mb-4 self-start">{`${power} // ${quantity === 1 ? 'Single' : `Packet of ${quantity}`}`}</p>
       </div>
-      <h1>{name}</h1>
-      <p>{`${power} // ${quantity === 1 ? 'Single' : `Packet of ${quantity}`}`}</p>
-      <AddToBasket price={price} productId={id}/>
-      <h2>Description</h2>
-      <p>{description}</p>
-      <h2>Specifications</h2>
-      <div>
-        <ul>
-          <li>
-            <p>Brand</p>
-            <p>{brand}</p>
-          </li>
-          <li>
-            <p>Item weight</p>
-            <p>{weight}</p>
-          </li>
-          <li>
-            <p>Dimensions</p>
-            <p>{`${height}x${width}x${length}`}</p>
-          </li>
-          <li>
-            <p>Item model number</p>
-            <p>{model_code}</p>
-          </li>
-          <li>
-            <p>Colour</p>
-            <p>{colour}</p>
-          </li>
-        </ul>
+      <div className="flex flex-col items-center w-screen">
+        <AddToBasket price={price} productId={id}/>
+        <div className="bg-blue text-white w-full p-8">
+          <h2 className="text-lg font-bold py-2">Description</h2>
+          <p className="text-xs md:text-sm">{description}</p>
+        </div>
+        <div className="bg-blue-dark text-white w-full p-8">
+          <h2 className="text-lg font-bold py-2">Specifications</h2>
+          <ul>
+            <li className="flex flex-row justify-start text-left">
+              <p className="w-1/2 py-2">Brand</p>
+              <p>{brand}</p>
+            </li>
+            <li className="flex flex-row">
+              <p className="w-1/2 py-2">Item weight</p>
+              <p>{weight}</p>
+            </li>
+            <li className="flex flex-row">
+              <p className="w-1/2 py-2">Dimensions</p>
+              <p>{`${height}x${width}x${length}`}</p>
+            </li>
+            <li className="flex flex-row">
+              <p className="w-1/2 py-2">Item model number</p>
+              <p>{model_code}</p>
+            </li>
+            <li className="flex flex-row">
+              <p className="w-1/2 py-2">Colour</p>
+              <p>{colour}</p>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
