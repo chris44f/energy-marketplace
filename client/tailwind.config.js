5b04aa73cd08f4ea512ccf16d2c93e82bb7de017
chrisfeatley <chris.featley@yodel.co.uk>
2022-04-28 11:23:31 +0100

    Added tailwind-css package, and implemented styling into index page

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'blue': '#150245',
+      'blue-dark': '#0e012e',
+      'blue-light': '#87edf6',
+      'counter-disabled': "#1a2a43",
+      'blue-accent': "#36598d",
+      'pink': '#df5ef0',
+      'white': '#ffffff',
+    },
+    fontSize: {
+      'sub-heading': '1.5rem',
+      'heading': '3rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
