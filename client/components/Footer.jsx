4f298e200c3c16d02839066f9823bb2f018be24c
Chris Featley <40803140+chris44f@users.noreply.github.com>
2022-08-17 15:16:35 +0100

    Update Footer.jsx

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
   return(
     <div className="block bg-blue text-blue-accent text-[14px] p-8">
-      Octopus Energy Ltd is a company registered in England and Wales.
+      This is some company information.
       <br />
-      Registered number: 09263424. Registered office: 33 Holborn, London, EC1N 2HT. Trading office: 20-24 Broadwick Street, London, W1F 8HT
+      This was created with Next.js
     </div>
   )
 }
